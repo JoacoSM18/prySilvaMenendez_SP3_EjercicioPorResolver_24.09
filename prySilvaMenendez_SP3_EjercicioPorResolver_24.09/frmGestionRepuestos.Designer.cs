6780f4e6c097dbf850beb91e6e7ac8be0d93a1b2
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-24 13:24:16 -0300

    Color e Icono

--- prySilvaMenendez_SP3_EjercicioPorResolver_24.09/frmGestionRepuestos.Designer.cs
+++ prySilvaMenendez_SP3_EjercicioPorResolver_24.09/frmGestionRepuestos.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGestionRepuestos));
             this.mrcDatos = new System.Windows.Forms.GroupBox();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
@@ -191,9 +192,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
             this.ClientSize = new System.Drawing.Size(583, 351);
             this.Controls.Add(this.lblIngresar);
             this.Controls.Add(this.mrcDatos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmGestionRepuestos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gestion de Repuestos";

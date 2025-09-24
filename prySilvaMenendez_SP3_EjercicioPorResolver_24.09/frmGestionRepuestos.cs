cd9918bc52e5cc341fc35f5f27bfa4914df50fb7
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-24 12:51:52 -0300

    Agregar archivos de proyecto.

---
+++ prySilvaMenendez_SP3_EjercicioPorResolver_24.09/frmGestionRepuestos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySilvaMenendez_SP3_EjercicioPorResolver_24._09
+{
+    public partial class frmGestionRepuestos : Form
+    {
+        public frmGestionRepuestos()
+        {
+            InitializeComponent();
+        }
+    }
+}

896fb4a9f4963dae13dc3a7515ca649457543e6c
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-25 12:54:51 -0300

    Numero del Repuesto Falta, Datos Float Listo

--- prySilvaMenendez_SP3_EjercicioPorResolver_24.09/frmGestionRepuestos.Designer.cs
+++ prySilvaMenendez_SP3_EjercicioPorResolver_24.09/frmGestionRepuestos.Designer.cs
@@ -34,43 +34,50 @@
             this.lstMarcas = new System.Windows.Forms.ComboBox();
             this.btnImportado = new System.Windows.Forms.RadioButton();
             this.btnNacional = new System.Windows.Forms.RadioButton();
-            this.txtPrecio = new System.Windows.Forms.TextBox();
             this.txtDescripcion = new System.Windows.Forms.RichTextBox();
             this.lblDescripcion = new System.Windows.Forms.Label();
             this.lblPrecio = new System.Windows.Forms.Label();
-            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
             this.lblOrigen = new System.Windows.Forms.Label();
             this.lblMarca = new System.Windows.Forms.Label();
             this.lblIngresar = new System.Windows.Forms.Label();
             this.btnAceptar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.MaskedTextBox();
+            this.lblSigno = new System.Windows.Forms.Label();
             this.mrcDatos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.txtCantidad)).BeginInit();
             this.SuspendLayout();
             // 
             // mrcDatos
             // 
+            this.mrcDatos.Controls.Add(this.lblSigno);
+            this.mrcDatos.Controls.Add(this.txtPrecio);
             this.mrcDatos.Controls.Add(this.txtCantidad);
             this.mrcDatos.Controls.Add(this.lstMarcas);
             this.mrcDatos.Controls.Add(this.btnImportado);
             this.mrcDatos.Controls.Add(this.btnNacional);
-            this.mrcDatos.Controls.Add(this.txtPrecio);
             this.mrcDatos.Controls.Add(this.txtDescripcion);
             this.mrcDatos.Controls.Add(this.lblDescripcion);
             this.mrcDatos.Controls.Add(this.lblPrecio);
-            this.mrcDatos.Controls.Add(this.lblCantidad);
+            this.mrcDatos.Controls.Add(this.lblNumero);
             this.mrcDatos.Controls.Add(this.lblOrigen);
             this.mrcDatos.Controls.Add(this.lblMarca);
-            this.mrcDatos.Location = new System.Drawing.Point(50, 68);
+            this.mrcDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mrcDatos.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mrcDatos.Location = new System.Drawing.Point(40, 105);
+            this.mrcDatos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.mrcDatos.Name = "mrcDatos";
-            this.mrcDatos.Size = new System.Drawing.Size(487, 221);
+            this.mrcDatos.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.mrcDatos.Size = new System.Drawing.Size(801, 342);
             this.mrcDatos.TabIndex = 0;
             this.mrcDatos.TabStop = false;
             this.mrcDatos.Text = "Datos a Ingresar";
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(114, 142);
+            this.txtCantidad.Location = new System.Drawing.Point(214, 218);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCantidad.Maximum = new decimal(new int[] {
             999999,
             0,
@@ -78,7 +85,7 @@
             0});
             this.txtCantidad.Name = "txtCantidad";
             this.txtCantidad.ReadOnly = true;
-            this.txtCantidad.Size = new System.Drawing.Size(67, 20);
+            this.txtCantidad.Size = new System.Drawing.Size(100, 28);
             this.txtCantidad.TabIndex = 3;
             this.txtCantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -90,18 +97,20 @@
             "P ( Peugeot )",
             "F ( Fiat )",
             "R ( Renault )"});
-            this.lstMarcas.Location = new System.Drawing.Point(114, 36);
+            this.lstMarcas.Location = new System.Drawing.Point(214, 55);
+            this.lstMarcas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.lstMarcas.Name = "lstMarcas";
-            this.lstMarcas.Size = new System.Drawing.Size(132, 21);
+            this.lstMarcas.Size = new System.Drawing.Size(196, 30);
             this.lstMarcas.TabIndex = 0;
             this.lstMarcas.SelectedIndexChanged += new System.EventHandler(this.lstMarcas_SelectedIndexChanged);
             // 
             // btnImportado
             // 
             this.btnImportado.AutoSize = true;
-            this.btnImportado.Location = new System.Drawing.Point(114, 104);
+            this.btnImportado.Location = new System.Drawing.Point(214, 163);
+            this.btnImportado.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnImportado.Name = "btnImportado";
-            this.btnImportado.Size = new System.Drawing.Size(72, 17);
+            this.btnImportado.Size = new System.Drawing.Size(114, 26);
             this.btnImportado.TabIndex = 2;
             this.btnImportado.TabStop = true;
             this.btnImportado.Text = "Importado";
@@ -110,92 +119,96 @@
             // btnNacional
             // 
             this.btnNacional.AutoSize = true;
-            this.btnNacional.Location = new System.Drawing.Point(114, 72);
+            this.btnNacional.Location = new System.Drawing.Point(214, 111);
+            this.btnNacional.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnNacional.Name = "btnNacional";
-            this.btnNacional.Size = new System.Drawing.Size(67, 17);
+            this.btnNacional.Size = new System.Drawing.Size(105, 26);
             this.btnNacional.TabIndex = 1;
             this.btnNacional.TabStop = true;
             this.btnNacional.Text = "Nacional";
             this.btnNacional.UseVisualStyleBackColor = true;
             // 
-            // txtPrecio
-            // 
-            this.txtPrecio.Location = new System.Drawing.Point(114, 182);
-            this.txtPrecio.Name = "txtPrecio";
-            this.txtPrecio.Size = new System.Drawing.Size(120, 20);
-            this.txtPrecio.TabIndex = 4;
-            // 
             // txtDescripcion
             // 
-            this.txtDescripcion.Location = new System.Drawing.Point(285, 62);
+            this.txtDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.txtDescripcion.Location = new System.Drawing.Point(503, 97);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtDescripcion.MaxLength = 50;
             this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new System.Drawing.Size(179, 140);
+            this.txtDescripcion.Size = new System.Drawing.Size(266, 213);
             this.txtDescripcion.TabIndex = 5;
             this.txtDescripcion.Text = "";
             // 
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(309, 36);
+            this.lblDescripcion.Location = new System.Drawing.Point(524, 55);
+            this.lblDescripcion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(129, 13);
+            this.lblDescripcion.Size = new System.Drawing.Size(215, 22);
             this.lblDescripcion.TabIndex = 4;
             this.lblDescripcion.Text = "Descripcion del Repuesto";
             // 
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(33, 189);
+            this.lblPrecio.Location = new System.Drawing.Point(85, 283);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.Size = new System.Drawing.Size(61, 22);
             this.lblPrecio.TabIndex = 3;
             this.lblPrecio.Text = "Precio";
             // 
-            // lblCantidad
+            // lblNumero
             // 
-            this.lblCantidad.AutoSize = true;
-            this.lblCantidad.Location = new System.Drawing.Point(7, 149);
-            this.lblCantidad.Name = "lblCantidad";
-            this.lblCantidad.Size = new System.Drawing.Size(101, 13);
-            this.lblCantidad.TabIndex = 2;
-            this.lblCantidad.Text = "Cantidad Requerida";
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(26, 224);
+            this.lblNumero.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(184, 22);
+            this.lblNumero.TabIndex = 2;
+            this.lblNumero.Text = "Numero del Repuesto";
             // 
             // lblOrigen
             // 
             this.lblOrigen.AutoSize = true;
-            this.lblOrigen.Location = new System.Drawing.Point(32, 89);
+            this.lblOrigen.Location = new System.Drawing.Point(85, 139);
+            this.lblOrigen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblOrigen.Name = "lblOrigen";
-            this.lblOrigen.Size = new System.Drawing.Size(38, 13);
+            this.lblOrigen.Size = new System.Drawing.Size(64, 22);
             this.lblOrigen.TabIndex = 1;
             this.lblOrigen.Text = "Origen";
             // 
             // lblMarca
             // 
             this.lblMarca.AutoSize = true;
-            this.lblMarca.Location = new System.Drawing.Point(7, 44);
+            this.lblMarca.Location = new System.Drawing.Point(87, 63);
+            this.lblMarca.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMarca.Name = "lblMarca";
-            this.lblMarca.Size = new System.Drawing.Size(103, 13);
+            this.lblMarca.Size = new System.Drawing.Size(59, 22);
             this.lblMarca.TabIndex = 0;
-            this.lblMarca.Text = "Marca del Repuesto";
+            this.lblMarca.Text = "Marca";
             // 
             // lblIngresar
             // 
             this.lblIngresar.AutoSize = true;
             this.lblIngresar.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIngresar.Location = new System.Drawing.Point(113, 20);
+            this.lblIngresar.Location = new System.Drawing.Point(170, 31);
+            this.lblIngresar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblIngresar.Name = "lblIngresar";
-            this.lblIngresar.Size = new System.Drawing.Size(357, 32);
+            this.lblIngresar.Size = new System.Drawing.Size(535, 48);
             this.lblIngresar.TabIndex = 1;
             this.lblIngresar.Text = "Ingrese los Datos del Repuesto";
             // 
             // btnAceptar
             // 
             this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAceptar.Location = new System.Drawing.Point(362, 304);
+            this.btnAceptar.Location = new System.Drawing.Point(543, 468);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(108, 35);
+            this.btnAceptar.Size = new System.Drawing.Size(162, 54);
             this.btnAceptar.TabIndex = 2;
             this.btnAceptar.Text = "&ACEPTAR";
             this.btnAceptar.UseVisualStyleBackColor = true;
@@ -204,25 +217,47 @@
             // btnCancelar
             // 
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelar.Location = new System.Drawing.Point(119, 304);
+            this.btnCancelar.Location = new System.Drawing.Point(178, 468);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(117, 35);
+            this.btnCancelar.Size = new System.Drawing.Size(176, 54);
             this.btnCancelar.TabIndex = 3;
             this.btnCancelar.Text = "&CANCELAR";
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(214, 277);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 28);
+            this.txtPrecio.TabIndex = 6;
+            this.txtPrecio.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txtPrecio_MaskInputRejected);
+            this.txtPrecio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecio_KeyPress);
+            this.txtPrecio.Leave += new System.EventHandler(this.txtPrecio_Leave);
+            // 
+            // lblSigno
+            // 
+            this.lblSigno.AutoSize = true;
+            this.lblSigno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSigno.Location = new System.Drawing.Point(187, 283);
+            this.lblSigno.Name = "lblSigno";
+            this.lblSigno.Size = new System.Drawing.Size(21, 22);
+            this.lblSigno.TabIndex = 7;
+            this.lblSigno.Text = "$";
+            // 
             // frmGestionRepuestos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Peru;
-            this.ClientSize = new System.Drawing.Size(583, 351);
+            this.ClientSize = new System.Drawing.Size(874, 540);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.lblIngresar);
             this.Controls.Add(this.mrcDatos);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmGestionRepuestos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gestion de Repuestos";
@@ -239,10 +274,9 @@
         private System.Windows.Forms.GroupBox mrcDatos;
         private System.Windows.Forms.Label lblDescripcion;
         private System.Windows.Forms.Label lblPrecio;
-        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblNumero;
         private System.Windows.Forms.Label lblOrigen;
         private System.Windows.Forms.Label lblMarca;
-        private System.Windows.Forms.TextBox txtPrecio;
         private System.Windows.Forms.RichTextBox txtDescripcion;
         private System.Windows.Forms.RadioButton btnImportado;
         private System.Windows.Forms.RadioButton btnNacional;
@@ -251,6 +285,8 @@
         private System.Windows.Forms.NumericUpDown txtCantidad;
         private System.Windows.Forms.Button btnAceptar;
         private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.MaskedTextBox txtPrecio;
+        private System.Windows.Forms.Label lblSigno;
     }
 }
 
--- prySilvaMenendez_SP3_EjercicioPorResolver_24.09/frmGestionRepuestos.cs
+++ prySilvaMenendez_SP3_EjercicioPorResolver_24.09/frmGestionRepuestos.cs
@@ -32,7 +32,7 @@ namespace prySilvaMenendez_SP3_EjercicioPorResolver_24._09
                 string.IsNullOrWhiteSpace(txtDescripcion.Text)
                )
             {
-                MessageBox.Show("Los Datos estan Incompletos");
+                MessageBox.Show("Los Datos estan Incompletos, Por Favor Complete Todos los Campos");
             }
             else
             {
@@ -43,6 +43,49 @@ namespace prySilvaMenendez_SP3_EjercicioPorResolver_24._09
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void txtPrecio_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void txtPrecio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)
+                && !char.IsDigit(e.KeyChar)
+                && e.KeyChar != '.'
+                && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '.' || e.KeyChar == ',')
+                && (txtPrecio.Text.Contains(".") || 
+                txtPrecio.Text.Contains(",")))
+            {
+                e.Handled = true;
+            }
+            string Precio = txtPrecio.Text;
+            if (Precio.Contains(".") || Precio.Contains(","))
+            {
+                int indexDecimal = Precio.IndexOf('.') >= 0 ? Precio.IndexOf('.') : Precio.IndexOf(',');
+
+                if (Precio.Length - indexDecimal > 2 && !char.IsControl(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void txtPrecio_Leave(object sender, EventArgs e)
+        {
+            string texto = txtPrecio.Text.Replace('.', ',');
+
+            if (!float.TryParse(texto, out float valor))
+            {
+                MessageBox.Show("Debe Ingresar un Número Válido (Formato Decimal)");
+                txtPrecio.Focus();
+            }
         }
     }
 }
